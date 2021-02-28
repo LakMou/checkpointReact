84bb8eb7c64237c9dab6f2c7e7df46464f27d730
LakehalMounir <lmou.3035@gmail.com>
2021-02-28 10:10:44 +0100

    checkpointReact

---
+++ checkpointReact/src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import  algImage from "./srcImage.jpg";
+import './App.css';
+
+
+function App() {
+  return (
+  <div className="App">
+    <div  style={{border:"solid 1px black",maxWidth:"100vw"}} >
+
+<h1 className="title red">Free Algeria</h1>
+
+<br />
+
+<img src={algImage} />
+
+<br />
+
+<img src="/imgInPublic.jpg" />
+
+
+</div>
+<video width="320" height="240" controls/>
+
+<source src="myVideo.mp4" type="video/mp4" />
+
+<video/>
+
+</div>
+  );
+ 
+}
+
+export default App;
